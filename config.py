f87c401dd39d07702c09231e3a60c578f56d9bef
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-28 23:17:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 class Config:                                                                   
     API_ID = int(os.environ.get("API_ID", "1738777"))
     API_HASH = os.environ.get("API_HASH", "028f44575fc5e5ca13bdeb0b7b3f603d")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5967480778:AAGyf60mypedqAtD5wc5z3lg0Y911jUZ_Hc")
     BOT_SESSION = os.environ.get("BOT_SESSION", "asuvarisubot")
     OWNER_ID = os.environ.get("OWNER_ID", "880087645")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "")  
