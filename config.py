fafc3b8e0b70b90fb54570e4064ea23483ed6c72
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-01-28 23:16:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
+    API_ID = int(os.environ.get("API_ID", "1738777"))
+    API_HASH = os.environ.get("API_HASH", "028f44575fc5e5ca13bdeb0b7b3f603d")       
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_SESSION = os.environ.get("BOT_SESSION", "")
-    OWNER_ID = os.environ.get("OWNER_ID", "")                             
+    BOT_SESSION = os.environ.get("BOT_SESSION", "asuvarisubot")
+    OWNER_ID = os.environ.get("OWNER_ID", "880087645")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001188070894"))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "asuvarisubot")
 
 
 def LOGGER(name: str) -> logging.Logger:
